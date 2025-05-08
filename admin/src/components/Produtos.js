35b31fe2e33bc9ac6a4c870d531789f440d91bf8
giovanehernandes <giovan3e@gmail.com>
2025-05-08 14:18:21 -0300

    Sobe o Arquivo Produtos

---
+++ admin/src/components/Produtos.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+
+const Produtos = () => {
+  const BuscarProdutos = async () => {
+    //axios.get, post, put, delete, patch
+    var token = localStorage.getItem("ALUNO_ITE");
+
+    var url = "https://backend-aula.vercel.app/app/produtos";
+    await axios
+      .get(url, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((retorno) => {
+        console.log(retorno);
+      });
+  };
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <input type="button" value="Buscar" onClick={() => BuscarProdutos()} />
+    </div>
+  );
+};
+
+export default Produtos;
